efe0f0520e9262936191c37feb9d3e5541c65a24
noval232 <adilaga.noval@gmail.com>
2025-09-18 11:04:16 +0700

    server to index.js and move package.json backend to insede backend folder

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,16 @@
-import ekspres from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
 
 dotenv.config();
-connectDB();
+connectDB(); // Pastikan ini ada
 
-const app = ekspres();
-const __dirname = path.resolve();
+const app = express();
 
-app.use(ekspres.json());
+app.use(express.json());
+
 app.use('/api/products', productRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(ekspres.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-  });
-}
-
+// Ini adalah satu-satunya yang kamu butuhkan untuk Vercel
 export default app;
